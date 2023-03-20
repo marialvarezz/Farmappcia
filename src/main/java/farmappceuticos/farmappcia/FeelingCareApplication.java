6ebda7b8ded8713a6c64f12ffc35a8bc54bc2fa0
marialvarezz <123487313+marialvarezz@users.noreply.github.com>
2023-03-21 00:12:21 +0100

    Base del proyecto creada

---
+++ src/main/java/farmappceuticos/farmappcia/FeelingCareApplication.java
@@ -0,0 +1,13 @@
+package farmappceuticos.farmappcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelingCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelingCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/farmappceuticos/farmappcia/FeelingCareApplicationTests.java
@@ -0,0 +1,13 @@
+package farmappceuticos.farmappcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelingCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
