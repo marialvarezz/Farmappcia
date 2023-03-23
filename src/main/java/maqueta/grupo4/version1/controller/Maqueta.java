d0f7660599de03c419ebd0b534e438309d262fb2
haraldjm <haraldjm@gmail.com>
2023-03-23 18:23:13 +0100

    Maqueta página inicio, menu usuario y gestión de medicamentos

---
+++ src/main/java/maqueta/grupo4/version1/controller/Maqueta.java
@@ -0,0 +1,18 @@
+package maqueta.grupo4.version1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Maqueta {
+    @GetMapping("/")
+    public String vistaInicial() {return "index";}
+    @GetMapping("/login")
+    public String vistaLogin() {return "login";}
+    @GetMapping("/usuarios/logout")
+    public String vistaLogout() {return "logout";}
+    @GetMapping("/usuarios/historialmedico")
+    public String vistaHistorialMedico() {return "historialmedico";}
+    @GetMapping("/usuarios/datosdelusuario")
+    public String vistaDatosDelUsuario() {return "datosdelusuario";}
+}
