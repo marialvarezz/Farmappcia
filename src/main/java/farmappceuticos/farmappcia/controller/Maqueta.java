333720e7e3699fb18aafcf30621f07932698f108
marialvarezz <123487313+marialvarezz@users.noreply.github.com>
2023-04-11 17:41:05 +0200

    HTMLs unidos al layout

--- src/main/java/farmappceuticos/farmappcia/controller/Maqueta.java
+++ src/main/java/farmappceuticos/farmappcia/controller/Maqueta.java
@@ -6,25 +6,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Maqueta {
     @GetMapping("/")
-    public String vistaInicial() {return "index3";}
+    public String vistaInicial() {return "home/index3";}
     @GetMapping("/login")
-    public String vistaLogin() {return "login";}
+    public String vistaLogin() {return "home/login";}
     @GetMapping("/logout")
-    public String vistaLogout() {return "logout";}
+    public String vistaLogout() {return "home/logout";}
     @GetMapping("/medicamentos")
-    public String catMedIni() {return "catalogomedicamentosinicio";}
+    public String catMedIni() {return "home/catalogomedicamentosinicio";}
     @GetMapping("/incompatibles")
-    public String incompatiblies() {return "incompatibilidadesmedicamentosinicio";}
+    public String incompatiblies() {return "home/incompatibilidadesmedicamentosinicio";}
     @GetMapping("/aboutus")
-    public String about() {return "about";}
+    public String about() {return "home/about";}
     @GetMapping("/contacto")
-    public String contactus() {return "contacto";}
+    public String contactus() {return "home/contacto";}
     @GetMapping("/noticias")
-    public String noticias() {return "noticias";}
+    public String noticias() {return "home/noticias";}
     @GetMapping("/hospitalescercanos")
-    public String hospitales() {return "hospitales";}
+    public String hospitales() {return "home/hospitales";}
     @GetMapping("/consejos")
-    public String consejos() {return "consejosf";}
+    public String consejos() {return "home/consejosf";}
 
 
 }
