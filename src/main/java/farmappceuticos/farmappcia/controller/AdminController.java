b4f4f544cdf21cf328c1d74600a62ec73b087f70
haraldjm <haraldjm@gmail.com>
2023-05-13 19:23:34 +0200

    Paginación de Medicamentos Admin.

--- src/main/java/farmappceuticos/farmappcia/controller/AdminController.java
+++ src/main/java/farmappceuticos/farmappcia/controller/AdminController.java
@@ -8,14 +8,19 @@ import farmappceuticos.farmappcia.services.MedicineService;
 import farmappceuticos.farmappcia.services.UserService1;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
@@ -71,9 +76,25 @@ public class AdminController {
 
     @GetMapping({"/medicamentos/"})
     //Model es el objeto que utiliza Spring para pasar al html los datos de la BD
-    public String showMedicines(Model model) {
+    public String showMedicines(Model model,
+                                @RequestParam("page")Optional<Integer> page,
+                                @RequestParam("size") Optional<Integer> size) {
         //
-        model.addAttribute("medicine", medicineService.findAll());
+        List<Medicine> medicines=medicineService.findAll();
+        model.addAttribute("medicine",medicines);
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(15);
+
+        Page<Medicine> medicinePageAdmin = medicineService.findAll(PageRequest.of(currentPage - 1, pageSize));
+
+        model.addAttribute("medicinePageAdmin", medicinePageAdmin);
+        int totalPages = medicinePageAdmin.getTotalPages();
+        if (totalPages > 0){
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
         //Devuelve el HTML
         return "medicine/admin-medicine-list";
     }
