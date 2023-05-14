8a519caa8d7856197f7d0968cdb97dd6bfb94aff
Cristina2493 <123487312+Cristina2493@users.noreply.github.com>
2023-05-14 19:21:09 +0200

    FAR-72- Paginación de preguntas

--- src/main/java/farmappceuticos/farmappcia/services/QuestionsService.java
+++ src/main/java/farmappceuticos/farmappcia/services/QuestionsService.java
@@ -1,12 +1,20 @@
 package farmappceuticos.farmappcia.services;
 
+import farmappceuticos.farmappcia.model.Illness;
 import farmappceuticos.farmappcia.model.Questions;
 import farmappceuticos.farmappcia.model.User;
 import farmappceuticos.farmappcia.repositories.QuestionsRepository;
 import farmappceuticos.farmappcia.repositories.UserRepository;
+import farmappceuticos.farmappcia.utils.IllnessUtils;
+import farmappceuticos.farmappcia.utils.QuestionsUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,4 +27,24 @@ public class QuestionsService extends AbstractBusinessServiceSoloEnt <Questions,
         super(questionsRepository);
     }
     //Implementación de los métodos principales que se usan al entrar en el CRUD
+
+    final private List<Questions> questions = QuestionsUtils.buildQuestions();
+    public Page<Questions> findPaginated(Pageable pageable) {
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<Questions> list;
+
+        if (questions.size() < startItem){
+            list = Collections.emptyList();
+        }else {
+            int toIndex = Math.min(startItem  + pageSize, questions.size());
+            list = questions.subList(startItem, toIndex);
+        }
+        Page<Questions> questionsPage = new PageImpl<Questions>(list, PageRequest.of(currentPage,pageSize), questions.size());
+
+        return questionsPage;
+
+    }
+
 }
